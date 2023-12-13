85c6a986d80a120e94a26556beb14ba0595d1186
Kinartb <arturomho8x@gmail.com>
2023-12-13 09:43:24 -0500

    Hasta parte 3 terminada

---
+++ app/models/movie.rb
@@ -0,0 +1,16 @@
+class Movie < ActiveRecord::Base
+    def self.all_ratings
+      ['G', 'PG', 'PG-13', 'R']
+    end
+    
+    def self.with_ratings(ratings, sort_by)
+      if ratings.nil?
+        all.order sort_by
+      else
+        where(rating: ratings.map(&:upcase)).order sort_by
+      end
+    end
+    def self.find_in_tmdb(search_terms)    
+    end  
+end
+  
